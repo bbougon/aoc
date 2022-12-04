370409014f08977cb463c1a93691d744aadcfabb
Bertrand Bougon <bertrandbougon@gmail.com>
2022-12-04 01:37:05 +0100

    Day 2 - part 2

--- src/day_2.rs
+++ src/day_2.rs
@@ -3,9 +3,41 @@ use std::cmp::Ordering;
 use std::cmp::Ordering::{Greater, Less};
 use std::fmt::{Display, Formatter};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, PartialEq)]
+enum Issue {
+    Draw,
+    Lose,
+    Win,
+}
+
+#[derive(Debug, PartialEq)]
+enum ShapeName {
+    Scissors,
+    Paper,
+    Rock,
+}
+
+#[derive(Debug)]
+struct UnencryptedShape {
+    name: Issue,
+}
+
+impl TryFrom<&str> for UnencryptedShape {
+    type Error = ();
+
+    fn try_from(shape: &str) -> Result<Self, Self::Error> {
+        match shape {
+            "Y" => Ok(UnencryptedShape { name: Issue::Draw }),
+            "X" => Ok(UnencryptedShape { name: Issue::Lose }),
+            "Z" => Ok(UnencryptedShape { name: Issue::Win }),
+            &_ => Err(()),
+        }
+    }
+}
+
+#[derive(Debug)]
 struct Shape {
-    name: Box<str>,
+    name: ShapeName,
     points: u32,
 }
 
@@ -33,16 +65,12 @@ impl Display for Shape {
 
 impl PartialOrd for Shape {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        if self.name == "Rock".into() && other.name == "Scissors".into() {
-            return Some(Greater);
-        }
-        if self.name == "Scissors".into() && other.name == "Paper".into() {
-            return Some(Greater);
-        }
-        if self.name == "Paper".into() && other.name == "Rock".into() {
-            return Some(Greater);
-        }
-        return Some(Less);
+        return match (&self.name, &other.name) {
+            (ShapeName::Rock, ShapeName::Scissors) => Some(Greater),
+            (ShapeName::Scissors, ShapeName::Paper) => Some(Greater),
+            (ShapeName::Paper, ShapeName::Rock) => Some(Greater),
+            _ => Some(Less),
+        };
     }
 }
 
@@ -58,7 +86,7 @@ impl Default for Scissors {
     fn default() -> Self {
         Scissors {
             0: Shape {
-                name: "Scissors".into(),
+                name: ShapeName::Scissors,
                 points: 3,
             },
         }
@@ -71,7 +99,7 @@ impl Default for Paper {
     fn default() -> Self {
         Paper {
             0: Shape {
-                name: "Paper".into(),
+                name: ShapeName::Paper,
                 points: 2,
             },
         }
@@ -84,14 +112,49 @@ impl Default for Rock {
     fn default() -> Self {
         Rock {
             0: Shape {
-                name: "Rock".into(),
+                name: ShapeName::Rock,
                 points: 1,
             },
         }
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
+struct UnencryptedRound<'a>((&'a Shape, &'a UnencryptedShape));
+
+impl<'a> UnencryptedRound<'a> {
+    fn score(&self) -> Result<u32, ()> {
+        match self.0 {
+            (opponent, me) => Ok(self.play(opponent, me)),
+        }
+    }
+
+    fn play(&self, opponent: &Shape, me: &UnencryptedShape) -> u32 {
+        return match me.name {
+            Issue::Draw => opponent.points + 3,
+            Issue::Lose => Self::lose_over(opponent) + 0,
+            Issue::Win => Self::win_over(opponent) + 6,
+        };
+    }
+
+    fn lose_over(opponent: &Shape) -> u32 {
+        return match opponent.name {
+            ShapeName::Scissors => 2,
+            ShapeName::Paper => 1,
+            ShapeName::Rock => 3,
+        };
+    }
+
+    fn win_over(opponent: &Shape) -> u32 {
+        return match opponent.name {
+            ShapeName::Scissors => 1,
+            ShapeName::Paper => 3,
+            ShapeName::Rock => 2,
+        };
+    }
+}
+
+#[derive(Debug)]
 struct Round<'a>((&'a Shape, &'a Shape));
 
 impl<'a> Round<'a> {
@@ -133,17 +196,19 @@ impl Rounds {
 
 pub struct Day2 {
     crypted_score: u32,
+    unencrypted_score: u32,
 }
 
 impl Day2 {
     pub fn run(file_path: &String) -> Day2 {
         Day2 {
             crypted_score: Self::crypted_cheat_sheet(file_path).iter().sum(),
+            unencrypted_score: Self::unencrypted_cheat_sheet(file_path).iter().sum(),
         }
     }
 
     fn crypted_cheat_sheet(file_path: &String) -> Vec<u32> {
-        let rounds = Self::rounds(file_path)
+        Self::rounds(file_path)
             .0
             .into_iter()
             .map(|(opponent, me)| {
@@ -152,10 +217,24 @@ impl Day2 {
                     &Shape::try_from(me.as_str()).expect(&format!("me: {}", me)),
                 ))
                 .score()
-                .unwrap()
+                .expect("Should have scored")
             })
-            .collect::<Vec<u32>>();
-        rounds
+            .collect::<Vec<u32>>()
+    }
+
+    fn unencrypted_cheat_sheet(file_path: &String) -> Vec<u32> {
+        Self::rounds(file_path)
+            .0
+            .into_iter()
+            .map(|(opponent, me)| {
+                UnencryptedRound((
+                    &Shape::try_from(opponent.as_str()).expect(&format!("opponent: {}", opponent)),
+                    &UnencryptedShape::try_from(me.as_str()).expect(&format!("me: {}", me)),
+                ))
+                .score()
+                .expect("Should have scored")
+            })
+            .collect::<Vec<u32>>()
     }
 
     fn rounds(file_path: &String) -> Rounds {
@@ -167,7 +246,12 @@ impl Day2 {
 impl Display for Day2 {
     fn fmt(&self, formatter: &mut Formatter<'_>) -> std::fmt::Result {
         writeln!(formatter, "Day 2, Elves games:")?;
-        writeln!(formatter, "\t- High score: {}", self.crypted_score)?;
+        writeln!(formatter, "\t- High crypted score: {}", self.crypted_score)?;
+        writeln!(
+            formatter,
+            "\t- High unencrypted score: {}",
+            self.unencrypted_score
+        )?;
         Ok(())
     }
 }
@@ -193,6 +277,26 @@ mod rock_paper_scissors {
         assert_eq!(
             Day2::run(&String::from("resources/tests/day_2_004.txt")).crypted_score,
             45
+        );
+    }
+
+    #[test]
+    fn should_give_winner_unencrypted_score() {
+        assert_eq!(
+            Day2::run(&String::from("resources/tests/day_2_001.txt")).unencrypted_score,
+            4
+        );
+        assert_eq!(
+            Day2::run(&String::from("resources/tests/day_2_002.txt")).unencrypted_score,
+            12
+        );
+        assert_eq!(
+            Day2::run(&String::from("resources/tests/day_2_003.txt")).unencrypted_score,
+            15
+        );
+        assert_eq!(
+            Day2::run(&String::from("resources/tests/day_2_004.txt")).unencrypted_score,
+            42
         );
     }
 }
